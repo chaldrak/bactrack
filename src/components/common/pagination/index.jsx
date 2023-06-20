b63d680f3fdb969a9a4726f73bcfd359a9e09b73
Chaldrak <chaldrakus@gmail.com>
2023-06-20 07:03:54 +0100

    perf(details class page): Finir la pagination en faisant fonctionner les boutons next et prev.

--- src/components/common/pagination/index.jsx
+++ src/components/common/pagination/index.jsx
@@ -9,21 +9,30 @@ export default function Pagination({
   const totalPages = Math.ceil(totalItems / itemPerPage);
   const lastItemIndex = currentPage * itemPerPage;
   const firstItemIndex = lastItemIndex - itemPerPage + 1;
+
+  const previousPage = () => {
+    if (currentPage > 1) setCurrentPage((prev) => prev - 1);
+  };
+
+  const nextPage = () => {
+    if (currentPage < totalPages) setCurrentPage((prev) => prev + 1);
+  };
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          href="#"
+        <span
+          onClick={previousPage}
           className="relative inline-flex items-center rounded-md border border-slate-500 px-4 py-2 text-sm font-medium text-slate-700 hover:bg-slate-800 hover:text-slate-400"
         >
           Préc
-        </a>
-        <a
-          href="#"
+        </span>
+        <span
+          onClick={nextPage}
           className="relative inline-flex items-center rounded-md border border-slate-500 px-4 py-2 text-sm font-medium text-slate-700 hover:bg-slate-800 hover:text-slate-400"
         >
           Suiv
-        </a>
+        </span>
       </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
@@ -41,19 +50,12 @@ export default function Pagination({
             aria-label="Pagination"
           >
             <span
-              href="#"
-              className="relative inline-flex items-center px-2 py-2 text-slate-400 hover:text-slate-100 focus:z-20"
+              onClick={previousPage}
+              className="relative inline-flex items-center px-2 py-2 text-slate-400 hover:text-slate-100 focus:z-20 cursor-pointer"
             >
               <span className="sr-only">Previous</span>
               <BiChevronLeft size={20} aria-hidden="true" />
             </span>
-            {/* <span
-              href="#"
-              aria-current="page"
-              className="relative z-10 inline-flex items-center bg-sky-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 rounded-full"
-            >
-              1
-            </span> */}
             {Array.from({ length: totalPages }, (_, index) => index + 1).map(
               (item) => (
                 <span
@@ -63,22 +65,19 @@ export default function Pagination({
                       ? "z-10 bg-sky-600 text-white rounded-md"
                       : "text-slate-500  hover:text-slate-50"
                   }`}
-                  onClick={() => {
-                    setCurrentPage(item);
-                    localStorage.setItem("currentPage", `${item}`);
-                  }}
+                  onClick={() => setCurrentPage(item)}
                 >
                   {item}
                 </span>
               )
             )}
-            <a
-              href="#"
-              className="relative inline-flex items-center px-2 py-2 text-slate-400 hover:text-slate-100 focus:z-20"
+            <span
+              onClick={nextPage}
+              className="relative inline-flex items-center px-2 py-2 text-slate-400 hover:text-slate-100 focus:z-20 cursor-pointer"
             >
               <span className="sr-only">Next</span>
               <BiChevronRight size={20} aria-hidden="true" />
-            </a>
+            </span>
           </nav>
         </div>
       </div>
