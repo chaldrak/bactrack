c9e554c88da6be6f6c270907205d9b661ed28f1f
Chaldrak <chaldrakus@gmail.com>
2023-06-27 16:28:44 +0100

    feat(resultats): Ajouter l'option save in pdf.

--- src/components/section-results/index.jsx
+++ src/components/section-results/index.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ResultsTable from "../mui/table-results";
 import BaseTabs from "../common/base-tabs";
 import { sortInAlphabeticOrder } from "../../utils";
+import { useReactToPrint } from "react-to-print";
 
 const tabs = [
   { name: "Général", index: 0 },
@@ -25,16 +26,32 @@ const Results = ({ results, students }) => {
     default:
       break;
   }
+
+  const documentRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    content: () => documentRef.current,
+    documentTitle: `PNL_Solution_reporting`,
+  });
+
   return (
     <section className="">
-      <BaseTabs tabs={tabs} current={current} setCurrent={setCurrent} />
-      {current !== 3 && (
-        <ResultsTable
-          students={students}
-          results={sortInAlphabeticOrder(results)}
-        />
-      )}
-      {current === 3 && <div>Yo</div>}
+      <div ref={documentRef}>
+        <BaseTabs tabs={tabs} current={current} setCurrent={setCurrent} />
+        {current !== 3 && (
+          <ResultsTable
+            students={students}
+            results={sortInAlphabeticOrder(results)}
+          />
+        )}
+        {current === 3 && <div>Yo</div>}
+      </div>
+      <button
+        onClick={handlePrint}
+        className="fixed right-5 md:right-10 bottom-10 md:bottom-[75px] bg-sky-600 px-4 py-2 rounded-full hover:bg-sky-800"
+      >
+        Télécharger
+      </button>
     </section>
   );
 };
